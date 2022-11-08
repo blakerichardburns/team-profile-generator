66b3af280b891c57e07ef4859600ccb326d45e6a
Blake Burns <bburns@mysgi.com>
2022-11-08 11:24:14 -0500

    added css stylesheet for generated html

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -7,12 +7,13 @@ function generateHTML(managerArray, engineerArray, internArray) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../dist/style.css" />
     <title>The Team</title>
   </head>
 
     <body>
         <header>
-            <h1 class="navbar" style="background-color: #e3f2fd;">The Team</h1>
+            <h1 class="navbar">The Team</h1>
         </header>
         ${generateTeam(managerArray, engineerArray, internArray)}
     </body>
@@ -32,7 +33,7 @@ function generateTeam(managerArray, engineerArray, internArray) {
   };
 
   function generateManager(manager) {
-    return `<div class="card text-bg-primary mb-3" style="max-width: 18rem;">
+    return `<div class="card" id="manager-card">
   <div class="card-header">Manager</div>
   <div class="card-body">
   <h5 class="card-title">${manager.getName()}</h5>
@@ -46,7 +47,7 @@ function generateTeam(managerArray, engineerArray, internArray) {
   };
 
   function generateEngineer(engineer) {
-    return `<div class="card text-bg-success mb-3" style="max-width: 18rem;">
+    return `<div class="card" id="engineer-card">
   <div class="card-header">Engineer</div>
   <div class="card-body">
   <h5 class="card-title">${engineer.getName()}</h5>
@@ -60,7 +61,7 @@ function generateTeam(managerArray, engineerArray, internArray) {
   };
 
   function generateIntern(intern) {
-    return `<div class="card text-bg-warning mb-3" style="max-width: 18rem;">
+    return `<div class="card" id="intern-card">
   <div class="card-header">Intern</div>
   <div class="card-body">
   <h5 class="card-title">${intern.getName()}</h5>
